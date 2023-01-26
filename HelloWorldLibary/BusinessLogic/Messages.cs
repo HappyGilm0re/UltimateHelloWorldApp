c660f1ccab92a06486fc4f517f0ee737f2d4008e
Tony Gilmore <tony.gilmore@ASAZ-99VZ2M3>
2023-01-26 16:54:46 +0000

    Add project files.

---
+++ HelloWorldLibary/BusinessLogic/Messages.cs
@@ -0,0 +1,57 @@
+﻿using HelloWorldLibary.Models;
+using Microsoft.Extensions.Logging;
+using System.Data;
+using System.Text.Json;
+
+namespace HelloWorldLibary.BusinessLogic
+{
+    public class Messages
+    {
+        private readonly ILogger<Messages> _log;
+
+        public Messages(ILogger<Messages> log)
+        {
+            _log = log;
+        }
+
+        public string Greeting(string language)
+        {
+            string output = LookUpCustomText("Greeting", language);
+            return output;
+        }
+
+        private string LookUpCustomText(string key, string language)
+        {
+            JsonSerializerOptions options = new()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            try
+            {
+                List<CustomText>? messageSets = JsonSerializer
+               .Deserialize<List<CustomText>>
+               (
+                   File.ReadAllText("CustomText.json"), options
+               );
+
+                CustomText? messages = messageSets?.Where(x => x.Language == language).First();
+
+                if (messages is null)
+                {
+                    throw new NullReferenceException("The Specified language was not found in AppSettings.json.");
+                }
+
+                return messages.Tranlations[key];
+            }
+            catch(Exception ex)
+            {
+                _log.LogError("Error: Looking up the customer text", ex);
+                throw;
+            }
+           
+
+
+        }
+    }
+}
---
+++ HelloWorldLibary/Models/CustomText.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloWorldLibary.Models;
+
+internal class CustomText
+{
+    public string Language { get; set; }
+    public Dictionary<string, string> Tranlations { get; set; }
+}
